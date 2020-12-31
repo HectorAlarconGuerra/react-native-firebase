d7afcd0df20c7dbae83580b9a4ff6bb45c9e2b00
hectoralarcon <hectoralarconguerra@gmail.com>
2020-12-31 07:57:46 -0500

    guardar datos en el state

--- screens/CreateUserScreen.js
+++ screens/CreateUserScreen.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, TextInput, ScrollView, StyleSheet } from "react-native";
 
 const CreateUserScreen = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChangeText = (name, value) => {
+    setState({ ...state, [name]: value });
+  };
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.inputGroup}>
-        <TextInput placeholder="Name User" />
+        <TextInput
+          placeholder="Name User"
+          onChangeText={(value) => handleChangeText("name", value)}
+        />
       </View>
       <View style={styles.inputGroup}>
-        <TextInput placeholder="Email User" />
+        <TextInput
+          placeholder="Email User"
+          onChangeText={(value) => handleChangeText("email", value)}
+        />
       </View>
       <View style={styles.inputGroup}>
-        <TextInput placeholder="Phone User" />
+        <TextInput
+          placeholder="Phone User"
+          onChangeText={(value) => handleChangeText("phone", value)}
+        />
       </View>
       <View>
-        <Button title="Save User" />
+        <Button title="Save User" onPress={() => console.log(state)} />
       </View>
     </ScrollView>
   );
